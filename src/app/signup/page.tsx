956cbbccff20dfe8c598ad3629a0e3d01526cfb1
Syed-Roop-Ali-2729 <164709030+Syed-Roop-Ali-2729@users.noreply.github.com>
2024-11-09 08:52:31 +0500

    commit

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { FaUserLarge } from "react-icons/fa6";
 import { MdEmail } from "react-icons/md";
 import { RiLockPasswordFill } from "react-icons/ri";
@@ -9,7 +10,7 @@ export default function Page() {
         <h1 className="text-center text-3xl font-bold tracking-wide mb-6">
           Sign Up
         </h1>
-        <div className="flex flex-col gap-6 items-center justify-center ">
+        <form className="flex flex-col gap-6 items-center justify-center ">
           <div className="flex gap-2 items-center justify-center w-full">
             <FaUserLarge className="text-2xl" />
             <input
@@ -46,10 +47,17 @@ export default function Page() {
               className="border-2 px-2 py-2 w-full"
             />
           </div>
-          <button className="bg-purple-500 w-full rounded-lg py-2 text-gray-100 text-lg">
+          <button className="bg-purple-500 w-full rounded-lg py-2 text-gray-100 text-lg hover:bg-purple-600 transition-all duration-200">
             SignUp
           </button>
-        </div>
+
+          <p className="text-base text-gray-600">
+            Aready have an Account? goto{" "}
+            <Link href="/login" className="text-blue-600">
+              Login
+            </Link>
+          </p>
+        </form>
       </div>
     </div>
   );
